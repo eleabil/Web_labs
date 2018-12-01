9a05372822f44546f856bd61291c4dda920658ce
eleabil <elenitakey@gmail.com>
2018-12-01 20:27:07 +0200

    lab 9

--- Wimbledon/js/fans.js
+++ Wimbledon/js/fans.js
@@ -1,11 +1,17 @@
-'use strict';
-// lab 8  WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var useLocalStorage = false;
 
 window.addEventListener('load', function () {
-        console.log("page is loaded!");
+    if (useLocalStorage) {
         getFansFromLocalStorage()
+    } else {
+        getAllFansFromIndexedDb()
+    }
 
 });
+
+/*window.addEventListener('load', function () {
+    getFansFromLocalStorage()
+});*/
 
 function getFansFromLocalStorage() {
 
@@ -48,6 +54,56 @@ function getCurrentDate() {
         mm = '0' + mm
     }
     var datetime = dd + '.' + mm + '.' + yyyy + ' ' + time;
-
     return datetime
+}
+
+function getAllFansFromIndexedDb() {
+
+     var db;
+     var openRequest = indexedDB.open("wimbledon_db", 3);
+
+/*     openRequest.onupgradeneeded = function (e) {
+         var thisDb = e.target.result;
+         if (!thisDb.objectStoreNames.contains("fans")) {
+             thisDb.createObjectStore("fans", {autoIncrement: true});
+         }
+         alert('onupgradeneeded')
+     };*/
+
+    openRequest.onsuccess = function (e) {
+        alert("Running indexedDb success");
+        db = e.target.result;
+
+        var transaction = db.transaction(["fans"], "readonly");
+        transaction.objectStore("fans").openCursor().onsuccess = function (e) {
+            var cursor = e.target.result;
+
+            var template = document.getElementById("appeals");
+
+            if (cursor) {
+
+               // for (var i in cursor.value) {
+                    var fans = cursor.value;
+                    // var template = document.getElementById("appeals");
+                    template.innerHTML += "<div class=\"card col-sm-12 col-lg-12 mt-3 fan-appeal-card\" id=\"appeal\">\n" +
+                        "                <div class=\"card-body fanbox-border\" >\n" +
+                        "                    <p class=\"card-text\">" + fans + "</p>\n" +
+                        "                    <div class=\"row\">\n" +
+                        "                        <div class=\"col-md-3\">" + getCurrentDate() + "</div>\n" +
+                        "                        <div class=\"col-md-8 col-md-offset fan-rightstr\">Username</div>\n" +
+                        "                    </div>\n" +
+                        "                </div>\n" +
+                        "            </div>";
+
+                //}
+                cursor.continue();
+            }
+        }
+
+    };
+
+    openRequest.onerror = function (e) {
+        alert('Running indexedDb error: ' + e)
+    };
+
 }
